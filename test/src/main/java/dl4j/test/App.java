33c8af46f5c521fb961fd6e78256d3be360326b9
pdl <pdl@192.168.101.31>
2018-12-16 14:29:48 +0100

    Initial commit

---
+++ test/src/main/java/dl4j/test/App.java
@@ -0,0 +1,13 @@
+package dl4j.test;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
